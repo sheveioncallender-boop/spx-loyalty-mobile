fee52aafcdf3b207a14fa7275b64a2475e20c685
sheveioncallender-boop <sheveioncallender@gmail.com>
2026-09-13 16:49:14 -0400

    Add files via upload

---
+++ spx_loyalty_mobile/__manifest__.py
@@ -0,0 +1,24 @@
+{
+    'name': "Jenny's Rewards",
+    'version': '19.0.3.0.0',
+    'summary': "Jenny's customer cards, loyalty, messages and mobile app",
+    'author': 'SPXCORP LTD',
+    'license': 'LGPL-3',
+    'depends': ['website_sale_loyalty', 'pos_loyalty', 'pos_restaurant', 'portal', 'mail', 'auth_signup'],
+    'data': [
+        'security/ir.model.access.csv',
+        'security/rules.xml',
+        'views/mobile_views.xml',
+        'views/onboarding_views.xml',
+    ],
+    'assets': {
+        'web.assets_backend': [
+            'spx_loyalty_mobile/static/src/home/home.js',
+            'spx_loyalty_mobile/static/src/home/home.xml',
+            'spx_loyalty_mobile/static/src/home/home.css',
+        ],
+    },
+    'post_init_hook': 'preserve_existing_members',
+    'installable': True,
+    'application': True,
+}
