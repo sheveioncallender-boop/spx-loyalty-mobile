d8a6dddaf7d7c2011e48d75be2691379752d684e
sheveioncallender-boop <sheveioncallender@gmail.com>
2026-09-13 21:59:03 -0400

    Delete spx_loyalty_mobile directory

--- spx_loyalty_mobile/migrations/19.0.3.0.0/post-preserve-members.py
+++
@@ -1,6 +0,0 @@
-from odoo import api, SUPERUSER_ID
-from odoo.addons.spx_loyalty_mobile.hooks import preserve_existing_members
-
-
-def migrate(cr, version):
-    preserve_existing_members(api.Environment(cr, SUPERUSER_ID, {}))
--- spx_loyalty_mobile/migrations/19.0.3.0.2/post-link-account-mails.py
+++
@@ -1,6 +0,0 @@
-from odoo import api, SUPERUSER_ID
-
-
-def migrate(cr, version):
-    env = api.Environment(cr, SUPERUSER_ID, {})
-    env['spx.mobile.registration']._repair_account_mail_links()
